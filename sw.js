b5731347c3c407e418e27a719f0ee52e1a848516
KingAmada <sarkiamada@gmail.com>
2025-05-10 01:43:12 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('hotel-drinks-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/sw.js'
+        // Add other assets if hosted locally
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
